30f1995a8e71dabf2907383efdff054ad42801cb
unknown <abebe@.(none)>
2012-01-04 23:58:31 +0900

    'TaskCFinished'

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true,
+                    length: {minimum: 10, too_short: '商品名は10文字以上でなければなりません'}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
     message: 'はGIF、JPG、PNG画像のURLでなければなりません'
