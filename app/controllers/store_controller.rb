67a3e1be02fed67084593ae0d8196934a5de1ce8
unknown <abebe@.(none)>
2012-01-09 22:19:27 +0900

    Task D Finished

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = acccess_counter
   end
 
+  def acccess_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+      session[:counter] += 1
+  end
+  
 end
